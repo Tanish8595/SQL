62db7f334c6057aebd1b49551fcad25b592456c8
Tanish <164002011+Tanish8595@users.noreply.github.com>
2024-06-20 16:00:46 +0530

    Arithmetic and mathematical Operator

---
+++ ARITHMETIC AND MATHEMATICAL FUNCTION.sql
@@ -0,0 +1,60 @@
+
+# Arithmetic Operators
+select(24+60);
+
+select(100*20);
+
+Select(400-200);
+
+Select(450/50);
+
+# Mathematical Operators
+#1. Round :- on the basis of 0.5
+select round(12.96);
+
+select round(12.489);
+
+select round(45.123,1);
+
+select round(9.99,1);
+
+select round(12.816,1);
+
+# 2.Floor:- Round down
+
+select floor(12.9);
+
+#3. Ceil:- round up
+ select ceil(12.9);
+ 
+ # 4.Sqrt :- Show the Root value
+ SELECT sqrt(625);
+ 
+ #5. Power
+ SELECT power(25,2);
+ SELECT power(25,3);
+ 
+ #6. ABS :- IT IS USED TO CONVERT NEGATIVE INTO POSITIVE
+ SELECT abs(-25);
+ 
+ 
+ #7.SIGN :- SHOW THE DIRECTION NUMBER IF NO. IS NEGATIVE SHOW -1,ELSE SHOW 1
+ SELECT SIGN(-456);
+ SELECT SIGN(456);
+ 
+ # 8.MOD :- IT SHOW THE REMAINDER
+ SELECT MOD(25,3);
+ SELECT MOD(12,2);
+ 
+ #9. SUM
+ # 10. RAND
+ SELECT RAND();
+ 
+ SELECT ROUND(RAND()*100);
+ 
+ SELECT ROUND(RAND()*2000+1000);
+ 
+ SELECT ROUND(2000+RAND()*1000);
+ 
+ SELECT ROUND(6000+RAND()*4000);
+ 
